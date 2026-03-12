c8f21ea05ff484a1bdf7e5f695dda011704c9712
Vitor Arthur <129072713+VitorArtC@users.noreply.github.com>
2026-03-11 21:08:49 -0300

    first commit

---
+++ ML-2025/Services/Logs.cs
@@ -0,0 +1,16 @@
+﻿namespace ML_2025.Services
+{
+    public class Logs
+    {
+
+        public void GenerateLogRequest(string input)
+        {
+            var log = DateTime.Now.ToString("dd/MM/yyyy HH:mm"+" - "+"input");
+        }
+
+        public void GenerateLogRequestResponse(string input, string resposta)
+        {
+            var log = DateTime.Now.ToString("dd/MM/yyyy HH:mm" + " - " + "input"+ resposta);
+        }
+    }
+}
